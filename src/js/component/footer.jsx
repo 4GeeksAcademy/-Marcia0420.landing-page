6cac4e9545ba81b62b9c2eec9277e44c02006d7e
Marcia0420 <138834592+Marcia0420@users.noreply.github.com>
2023-08-08 18:24:57 +0000

    landing-page

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Footer=()=> {
+    return (
+        <div>
+     <br/>       
+<nav class="navbar bg-dark d-flex justify-content-center text-center">
+  <div class="container-fluid">
+    <p class="navbar-brand " >Marcia da Rosa</p>
+  </div>
+</nav>
+        </div>
+    )
+}
+export default Footer 
